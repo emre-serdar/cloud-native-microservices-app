8a4531ec7183089200136a9dea52c03069109393
emre-serdar <emreserdaar@gmail.com>
2024-10-11 15:52:33 -0400

    Add authentication service tests and update package.json, package-lock.json, and server.js

--- services/authentication-service/server.js
+++ services/authentication-service/server.js
@@ -1,6 +1,10 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
